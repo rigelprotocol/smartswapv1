2ba02dcad169d32b9e8196e9e44b6559c1113cd2
Ilamini Dagogo <dagogodboss@gmail.com>
2021-02-18 08:40:38 +0100

    first commit

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -32,15 +32,14 @@ export default function App() {
   return (
     <AppWrapper>
       <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
+        titleTemplate="%s - Rigel Smart-Swap"
+        defaultTitle="Rigel Smart-Swap"
       >
-        <meta name="description" content="A React.js Boilerplate application" />
+        <meta name="description" content="A Rigel Smart-Swap application" />
       </Helmet>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
