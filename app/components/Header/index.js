12775f9f00a79878632746d25143ae929deb97e1
Ilamini Dagogo <dagogodboss@gmail.com>
2021-02-18 08:48:28 +0100

    Remove default example

--- app/components/Header/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-function Header() {
-  return <div />;
-}
-
-export default Header;
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -2,56 +2,17 @@
  * HomePage
  *
  * This is the first thing users see of our App, at the '/' route
+ *
  */
 
-import React, { useEffect, memo } from 'react';
-import { Helmet } from 'react-helmet';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
 
-// @ts-ignore
-import { useInjectReducer } from 'utils/injectReducer';
-import { useInjectSaga } from 'utils/injectSaga';
-
-import reducer from './reducer';
-import saga from './saga';
-
-const key = 'home';
-
-// @ts-ignore
-export function HomePage() {
-  useInjectReducer({ key, reducer });
-  useInjectSaga({ key, saga });
-
-  useEffect(() => {
-    // When initial state username is not null, submit the form to load repos
-  }, []);
-
+export default function HomePage() {
   return (
-    <article>
-      <Helmet>
-        <title>Home Page</title>
-        <meta name="description" content="About Rigel Swap" />
-      </Helmet>
-    </article>
+    <h1>
+      <FormattedMessage {...messages.header} />
+    </h1>
   );
 }
-
-HomePage.propTypes = {};
-
-const mapStateToProps = createStructuredSelector({});
-
-export function mapDispatchToProps() {
-  return {};
-}
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-export default compose(
-  withConnect,
-  memo,
-)(HomePage);
