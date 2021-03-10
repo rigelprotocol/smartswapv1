2f6d554c1a7195a553841b4c4c1163494565d7de
OnahProsperity <prosperauthor@gmail.com>
2021-03-10 10:59:42 +0100

    pulled master branch

--- app/components/sendToken/index.js
+++ app/components/sendToken/index.js
@@ -1,9 +1,5 @@
 // @ts-nocheck
-<<<<<<< HEAD
 import { ethers } from 'ethers';
-=======
-import React from 'react';
->>>>>>> a4c3343f6512eaf74fd87c7cd1f2530650795d6e
 import { Box } from '@chakra-ui/layout';
 import {
   Flex,
@@ -20,52 +16,15 @@ import {
 } from '@chakra-ui/react';
 import { SettingsIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/button';
-<<<<<<< HEAD
 import React, {useState, useEffect} from 'react';
-=======
 import { Input } from '@chakra-ui/input';
->>>>>>> a4c3343f6512eaf74fd87c7cd1f2530650795d6e
 import ArrowDownImage from '../../assets/arrow-down.svg';
 import From from './from';
 import To from './to';
 import swapConnect, {provider, signer} from '../../utils/swapConnect';
 import SmartSwapRouter02 from '../../utils/abis/SmartSwapRouter02.json';
 
-<<<<<<< HEAD
-const Manual = () => {
- 
-  return (
-    <div>
-      <Box
-        boxShadow=" 0px 10px 20px  rgba(18, 1, 54,0.25)"
-        bg="#120136"
-        mt={3}
-        p={5}
-        rounded="2xl"
-      >
-        <From />
-        <Box textAlign="center">
-          <ArrowDownImage />
-        </Box>
-        <To />
-        <Box mt={14}>
-          <Button
-            d="block"
-            w="100%"
-            h="50px"
-            color="#40BAD5"
-            border="none"
-            fontWeight="regular"
-            fontSize="lg"
-            cursor="pointer"
-            rounded="2xl"
-            bg="rgba(64, 186, 213,0.25)"
-            borderColor="#40BAD5"
-            _hover={{ background: 'rgba(64, 186, 213,0.35)' }}
-            _active={{ outline: '#29235E', background: '#29235E' }}
-            onClick={() => {
-              console.log('Hellooo.....')
-=======
+             
 const Manual = () => (
   <div>
     <Box
@@ -100,7 +59,6 @@ const Manual = () => (
             style={{
               backgroundColor: 'rgb(41, 35, 94)',
               color: 'white',
->>>>>>> a4c3343f6512eaf74fd87c7cd1f2530650795d6e
             }}
           >
             <PopoverHeader fontWeight="semibold">
