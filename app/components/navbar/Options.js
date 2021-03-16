13883320af438951543c00aec38351b83fdb33fc
Ilamini Dagogo <dagogodboss@gmail.com>
2021-03-16 09:57:03 +0100

    Bug for Options.js

--- app/components/navbar/Options.js
+++ app/components/navbar/Options.js
@@ -2,14 +2,11 @@ import React, { useEffect } from 'react';
 import { useWeb3Context } from 'web3-react';
 import { Flex, Text } from '@chakra-ui/layout';
 import { ModalBody, ModalHeader } from '@chakra-ui/react';
-import { useInjectReducer } from 'utils/injectReducer';
-import reducer from 'containers/NoticeProvider/reducer';
 import MetaMaskImage from '../../assets/mask.svg';
 import TrustWalletImage from '../../assets/trust.svg';
 import { connectWallet } from '../../containers/WalletProvider/actions';
-import { notify } from '../../containers/NoticeProvider/actions';
+// import { notify } from '../../containers/NoticeProvider/actions';
 const Options = () => {
-  useInjectReducer({ key: 'NoticeProvider', reducer });
   const context = useWeb3Context();
   const connectMetaMask = () => {
     connectWallet();
