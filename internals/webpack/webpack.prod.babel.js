c20e323fe4eae9108b3cc6e9532b102f347940af
kayroy247 <okunladekayode@gmail.com>
2021-08-07 17:06:28 +0100

    clear cache

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -1,7 +1,7 @@
 // Important modules this config uses
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+
 
 const { HashedModuleIdsPlugin } = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -92,26 +92,7 @@ module.exports = require('./webpack.base.babel')({
       minRatio: 0.8,
     }),
 
-    new WebpackPwaManifest({
-      name: 'Rigel Smart Swap',
-      short_name: 'Rigel-SS',
-      description: 'Rigel Protocol Smart Swap',
-      background_color: '#fafafa',
-      theme_color: '#29235e',
-      inject: true,
-      ios: true,
-      icons: [
-        {
-          src: path.resolve('app/images/icon-512x512.png'),
-          sizes: [72, 96, 128, 144, 192, 384, 512],
-        },
-        {
-          src: path.resolve('app/images/icon-512x512.png'),
-          sizes: [120, 152, 167, 180],
-          ios: true,
-        },
-      ],
-    }),
+
 
     new HashedModuleIdsPlugin({
       hashFunction: 'sha256',
