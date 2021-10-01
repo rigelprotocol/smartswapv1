b408232ddbeaf81b1a83a06c07049f1a45bf3e01
Goodness Chris-Ugari <goodnesschrisugari@yahoo.com>
2021-10-01 07:59:42 +0100

    feat: display price history on the chart graph

--- app/components/charts/ChartGraph.js
+++ app/components/charts/ChartGraph.js
@@ -1,84 +1,89 @@
 import { Box, Flex, Text } from '@chakra-ui/layout';
 import styles from '../../styles/chart.css';
 import Chart from 'chart.js';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MinusIcon } from '@chakra-ui/icons';
+import axios from 'axios';
 
 const Graph = ({ setShow }) => {
   const ref = React.createRef();
+  const [rgpPrice, setRgpPrice] = useState([]);
+  const [volume24, setVolume24] = useState([]);
+  const getPriceHistory = () =>{
+  const chartRef = ref.current.getContext('2d');
+  const ctx = ref.current.getContext('2d');
+  const gradient = ctx.createLinearGradient(90, 245, 85, 30);
+  gradient.addColorStop(0.5, 'rgba(64, 186, 213,0)');
+  gradient.addColorStop(1, 'rgba(64, 186, 213,0.25)');
+  let rgpToBnb = [];
+    axios.get("https://api.coingecko.com/api/v3/coins/rigel-protocol/market_chart?vs_currency=bnb&days=30&interval=daily")
+      .then(res=>{
+        console.log(res.data);
+        console.log(res.data.prices[0][1])
+        for(const dataArray of res.data.prices){
+          rgpToBnb.push(dataArray[1])
+        }
+        setRgpPrice(rgpToBnb)
+        new Chart(chartRef, {
+          type: 'line',
+          data: {
+            labels: ['', '', '', '', '', '', '', ''],
+            datasets: [
+              {
+                fill: true,
+                lineTension: 0.5,
+                backgroundColor: gradient,
+                borderColor: '#40BAD5',
+                borderDashOffset: 0.0,
+                pointRadius: 0,
+                data: rgpToBnb,
+              },
+            ],
+          },
+          options: {
+            legend: {
+              display: false,
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              xAxes: [
+                {
+                  ticks: { display: false },
+                  gridLines: { display: false },
+                },
+              ],
+              yAxes: [
+                {
+                  display: true,
+                  position: 'right',
+                  ticks: {
+                    fontColor: 'rgba(255, 255, 255,0.25)',
+                    labels: rgpToBnb,
+                  },
+                  gridLines: {
+                    display: false,
+                  },
+                },
+                {
+                  display: false,
+                  position: 'left',
+                },
+              ],
+            },
+          },
+        });
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+  }
 
   useEffect(() => {
-    const chartRef = ref.current.getContext('2d');
-    const ctx = ref.current.getContext('2d');
-    const gradient = ctx.createLinearGradient(90, 245, 85, 30);
-    gradient.addColorStop(0.5, 'rgba(64, 186, 213,0)');
-    gradient.addColorStop(1, 'rgba(64, 186, 213,0.25)');
-
-    new Chart(chartRef, {
-      type: 'line',
-      data: {
-        labels: ['', '', '', '', '', '', '', ''],
-        datasets: [
-          {
-            fill: true,
-            lineTension: 0.5,
-            backgroundColor: gradient,
-            borderColor: '#40BAD5',
-            borderDashOffset: 0.0,
-            pointRadius: 0,
-            data: [0, 5, 4, 14, 10, 20, 5, 11],
-          },
-        ],
-      },
-      options: {
-        legend: {
-          display: false,
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          xAxes: [
-            {
-              ticks: { display: false },
-              gridLines: { display: false },
-            },
-          ],
-          yAxes: [
-            {
-              display: true,
-              position: 'right',
-              ticks: {
-                fontColor: 'rgba(255, 255, 255,0.25)',
-                min: 0.0,
-                max: 20.0,
-                stepSize: 10.0,
-                suggestedMin: 0.0,
-                suggestedMax: 20.0,
-                callback: (label) => {
-                  switch (label) {
-                    case 0.0:
-                      return '0.02 BNB';
-                    case 10.0:
-                      return '0.03 BNB';
-                    case 20.0:
-                      return '0.04 BNB';
-                  }
-                },
-              },
-              gridLines: {
-                display: false,
-              },
-            },
-            {
-              display: false,
-              position: 'left',
-            },
-          ],
-        },
-      },
-    });
+    getPriceHistory();
   }, []);
   return (
+
     <Box className={styles.charts__container}>
       <Flex
         color="gray.400"
@@ -95,7 +100,7 @@ const Graph = ({ setShow }) => {
         <div>
           <Text className={styles.text__market}>Current Price</Text>
           <Text className={styles.text}>
-            0.03892 BNB <span className={styles.decrease}>-0.03%</span>
+            {rgpPrice[rgpPrice.length - 1]} BNB <span className={styles.decrease}>-0.03%</span>
           </Text>
         </div>
         <MinusIcon
